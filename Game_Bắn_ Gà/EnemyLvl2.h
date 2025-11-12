d9558fa4ae29249c3a900da33e0afbf379901f7b
LeMinhChi <6551071011@st.utc2.edu.vn>
2025-11-12 22:45:38 +0700

    Update EnemyLvl2.h

--- Game_Bắn_ Gà/EnemyLvl2.h
+++ Game_Bắn_ Gà/EnemyLvl2.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "EnemyBase.h" // Kế thừa từ Lớp Cha
 
 // Lớp Quái Cấp 2
@@ -11,4 +11,6 @@ public:
     // Hàm Update (dùng "lời hứa" 4 tham số mới)
     virtual void update(float deltaTime, std::vector<Bullet>& enemyBullets,
         sf::Texture& enemyBulletTexture, float windowWidth) override;
+    virtual void shoot(std::vector<Bullet>& enemyBullets,
+        sf::Texture& enemyBulletTexture) override;
 };
