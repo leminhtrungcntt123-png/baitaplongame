d47f0c312aab9a4e18dfc40853016c67a5e8674f
LeMinhChi <6551071011@st.utc2.edu.vn>
2025-11-12 22:45:19 +0700

    Update EnemyLvl2.cpp

--- Game_Bắn_ Gà/EnemyLvl2.cpp
+++ Game_Bắn_ Gà/EnemyLvl2.cpp
@@ -1,4 +1,4 @@
-﻿#include "EnemyLvl2.h"
+#include "EnemyLvl2.h"
 
 // Hàm Dựng Lvl2:
 // Gọi hàm dựng Cha: 2 HP, Tốc độ 120
@@ -23,4 +23,22 @@ void EnemyLvl2::update(float deltaTime, std::vector<Bullet>& enemyBullets,
     // Chỉ cần gọi hàm của Cha. Cha sẽ tự di chuyển (vào Waypoint -> lượn ngang)
     // và tự gọi hàm shoot() (rỗng) của Cha.
     EnemyBase::update(deltaTime, enemyBullets, enemyBulletTexture, windowWidth);
+}
+void EnemyLvl2::shoot(std::vector<Bullet>& enemyBullets,
+    sf::Texture& enemyBulletTexture)
+{
+    // Lấy vị trí tâm của quái
+    sf::FloatRect bounds = this->sprite.getGlobalBounds();
+    sf::Vector2f pos(bounds.left + bounds.width / 2.f, bounds.top + bounds.height);
+
+    // Hướng bay (0, 1) = 0 X, 1 Y (đi xuống)
+    sf::Vector2f dir(0.f, 1.f);
+
+    // Sát thương (theo yêu cầu của bạn)
+    int damage = 2; // Quái Lvl3 gây 2 sát thương
+
+    float bulletSpeed = 400.f; // Tốc độ đạn quái
+
+    // Tạo viên đạn
+    enemyBullets.push_back(Bullet(enemyBulletTexture, pos, dir, bulletSpeed, damage));
 }
