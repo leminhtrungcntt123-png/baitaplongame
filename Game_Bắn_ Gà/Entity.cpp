b0bab81c54a6c99d30252b636a2cf4f3d5a831a8
LeMinhChi <6551071011@st.utc2.edu.vn>
2025-11-18 23:13:57 +0700

    Update Entity.cpp

--- Game_Bắn_ Gà/Entity.cpp
+++ Game_Bắn_ Gà/Entity.cpp
@@ -5,7 +5,8 @@ Entity::Entity(int initialHp, float initialSpeed)
     : Hp(initialHp),
     speed(initialSpeed),
     shootTimer(0.f),
-    shootCooldown(1.f) // (Đặt mặc định là 1 giây)
+    shootCooldown(1.f) ,// (Đặt mặc định là 1 giây)
+    mMaxHp(initialHp)
 {
     // Hàm dựng Cha không cần làm gì thêm
 }
@@ -45,4 +46,19 @@ void Entity::setInitialPosition(float x, float y)
 int Entity::getHP()
 {
     return this->Hp;
+}
+
+int Entity::getMaxHP() const
+{
+    return mMaxHp;
+}
+
+sf::Vector2f Entity::getCenterPosition() const
+{
+    // (Lấy "ranh giới" (bounds) "toàn cục" (global) "của" (of) "sprite" (sprite))
+    sf::FloatRect bounds = this->sprite.getGlobalBounds();
+
+    // (Tính toán "và" (and) "trả về" (return) "tọa độ" (coordinates) "trung tâm" (center))
+    return sf::Vector2f(bounds.left + bounds.width / 2.f,
+        bounds.top + bounds.height / 2.f);
 }
