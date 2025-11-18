8fe2ce0a5d889447bc3d28c95da4db398232815d
LeMinhChi <6551071011@st.utc2.edu.vn>
2025-11-18 23:22:13 +0700

    Update Player.h

--- Game_Bắn_ Gà/Player.h
+++ Game_Bắn_ Gà/Player.h
@@ -10,12 +10,15 @@ class Player : public Entity
 public:
     // Hàm Dựng (Truyền "chung" và "riêng")
     Player(sf::Texture& playerTexture, sf::Texture& bulletTexture,
-        std::vector<Bullet>& bullets, float bulletSpeed);
+        std::vector<Bullet>& bullets, float bulletSpeed,
+        sf::Texture& playerVIPTexture, sf::Texture& bulletVIPTexture);
     virtual ~Player() = default; // (Hàm Hủy "Ảo")
     void handleInput(sf::Event& event);
     bool update(float deltaTime, float windowWidth); // (Hàm này "viết đè" (override))
     void upgradeGun();
+    void megaUpgradeGun();
     void resetPosition();
+    void heal(int mount); // hàm Buff hp
 
 private:
     // (Hàm "riêng")
@@ -24,6 +27,12 @@ private:
     float mBulletSpeed; // (Vẫn giữ "riêng" vì EnemyBase không cần)
     sf::Vector2f mInitialPos;
 
+    float mHorizontalSpeed; // (Biến này bị thiếu)
+    float mVerticalSpeed;
+
+    sf::Texture& mPlayerVIPTextureRef;
+    sf::Texture& mBulletPlayerVIPTextureRef;
+    bool mIsUpgraded;
     // (Tham chiếu "riêng")
     sf::Texture& mBulletTextureRef;
     std::vector<Bullet>& mPlayerBulletsRef;
