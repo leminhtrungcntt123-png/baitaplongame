5d79c09a1af3afa35af648edac26f3ff6f2311a3
LeMinhChi <6551071011@st.utc2.edu.vn>
2025-11-18 23:25:03 +0700

    Update WaveManager.h

--- Game_Bắn_ Gà/WaveManager.h
+++ Game_Bắn_ Gà/WaveManager.h
@@ -6,6 +6,14 @@
 #include "EnemyLvl1.h"
 #include "EnemyLvl3.h"
 
+struct WaveData
+{
+    int enemyType;      // (Loại "1" (1) "là" (is) "Enemy1", "4" (4) "là" (is) "Boss", "5" (5) "là" (is) "Meteor" (Meteor)...)
+    int numEnemies;     // (Tổng "số" (number) "quái" (enemies) "sẽ" (will be) "tạo" (spawned))
+    float spawnInterval;// (Thời gian "chờ" (wait) "giữa" (between) "mỗi" (each) "lần" (time) "tạo" (spawn))
+    float waveTime;     // (Thời gian "tối đa" (maximum) "của" (of) "vòng" (wave) "đó" (that))
+};
+
 class WaveManager
 {
 public:
@@ -23,8 +31,10 @@ public:
         sf::Texture& enemy3Tex,
         sf::Texture& enemy2Tex,
         sf::Texture& bossTex,
-        sf::Texture& bossBulletTex);
+        sf::Texture& bossBulletTex,
+        sf::Texture& meteoriteTexture);
 
+    void setupWaves();
     // Hàm update (chứa toàn bộ logic spawn cũ)
     void update(float deltaTime);
 
@@ -33,6 +43,11 @@ public:
     bool isAllWavesFinished() const;
     State getState() const;       // Lấy trạng thái hiện tại (Đang Spawning? Intermission?)
     int getNextWaveNumber() const; // Lấy số của wave *tiếp theo*
+    float getRemainingTime() const; // Lấy thời gian còn lại
+    int getCurrentEnemyType() const; // Để biết đang là wave thường hay wave thiên thạch
+    void spawnEnemy();
+    int mDifficultyLevel; // 0 = Easy, 1 = Normal, 2 = Hard
+    void setDifficulty(int level);
 private:
     // --- CÁC BIẾN MÀ CHÚNG TA "CẮT" TỪ GAME.H ---
     float mEnemySpawnTimer;
@@ -47,6 +62,8 @@ private:
     float mWaveCooldown;  // Thời gian chờ giữa các wave
     float mWaveTimer;     // Đồng hồ đếm chờ
 
+    std::vector<WaveData> mWaves;
+
     // --- CÁC THAM CHIẾU ĐẾN "TÀI NGUYÊN" GỐC CỦA GAME ---
     std::vector<std::unique_ptr<EnemyBase>>& mEnemiesRef; // Tham chiếu tới mEnemies
     sf::Texture& mEnemy1TextureRef; // Tham chiếu tới mEnemy1Texture
@@ -54,4 +71,5 @@ private:
     sf::Texture& mEnemy2TextureRef;
     sf::Texture& mBossTextureRef;
     sf::Texture& mBossBulletTextureRef;
+    sf::Texture& mMeteoriteTextureRef;
 };
