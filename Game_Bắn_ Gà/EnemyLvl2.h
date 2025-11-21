105e413cfc9a076497154fc50a4bcca04e9d979b
leminhtrung <leminhtrungcntt123@gmail.com>
2025-11-21 23:15:09 +0700

    Include Animation class and add animation variable
    Added Animation class for enemy animations.

--- Game_Bắn_ Gà/EnemyLvl2.h
+++ Game_Bắn_ Gà/EnemyLvl2.h
@@ -1,16 +1,19 @@
 #pragma once
-#include "EnemyBase.h" // Kế thừa từ Lớp Cha
+#include "EnemyBase.h" 
+#include "Animation.h" // <--- THÊM: Include file Animation
 
-// Lớp Quái Cấp 2
 class EnemyLvl2 : public EnemyBase
 {
+protected:
+    // <--- THÊM: Khai báo biến Animation
+    Animation animationRun;
+
 public:
-    // Hàm Dựng
     EnemyLvl2(sf::Texture& texture, sf::Vector2f startPosition);
 
-    // Hàm Update (dùng "lời hứa" 4 tham số mới)
     virtual void update(float deltaTime, std::vector<Bullet>& enemyBullets,
         sf::Texture& enemyBulletTexture, float windowWidth) override;
+        
     virtual void shoot(std::vector<Bullet>& enemyBullets,
         sf::Texture& enemyBulletTexture) override;
 };
